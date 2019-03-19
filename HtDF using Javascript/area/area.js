a5580e071fcab710e96eee4fb35c3589165cf9c0
loki-one <lokesh.jadhav10@gmail.com>
2019-03-19 15:42:53 +0100

    add Area HtDF exercise and solution

---
+++ HtDF using Javascript/area/area.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+* PROBLEM:
+* DESIGN a function called area that consumes the length of one side 
+* of a square and produces the area of the square.
+* Remember, when we say DESIGN, we mean follow the recipe.
+* Leave behind commented out versions of the stub and template.
+*/
+
+// Number -> Number
+// produces the area of the square by multiplying the input number with itself
+
+// stub
+// function area (inputLength) {
+//     return 0;
+// }
+
+// template
+// function area (inputLength) {
+//     return (... inputLength);
+// }
+
+function area (inputLength) {
+    return (inputLength * inputLength);
+}
